33e3a1df53e8816f1c530c8589e2fad9c1c2f636
ilmi-veliu <iveliu011@gmail.com>
2025-04-10 13:48:44 +0200

    2

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Trie un tableau d'entiers en utilisant l'algorithme du tri par sélection
+ * @array: le tableau à trier
+ * @size: la taille du tableau
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, plus_petit_indices;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		plus_petit_indices = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[plus_petit_indices])
+				plus_petit_indices = j;
+		}
+
+		if (plus_petit_indices != i)
+		{
+			temp = array[i];
+			array[i] = array[plus_petit_indices];
+			array[plus_petit_indices] = temp;
+			print_array(array, size);
+		}
+	}
+}
