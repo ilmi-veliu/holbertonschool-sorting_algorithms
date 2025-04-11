6ac77803725023d058d1cfab75eaee070ebd94a8
ilmi-veliu <iveliu011@gmail.com>
2025-04-11 16:38:44 +0200

    0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+void bubble_sort(int *array, size_t size)
+{
+	int count;
+	int i = 0;
+
+	while (array[i])
+	{
+		i++;
+
+		
+	}
+}
--- sort.h
+++ sort.h
@@ -28,4 +28,5 @@ void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+
 #endif
